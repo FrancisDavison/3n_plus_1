43d87ead8a0c41dfdf77896769c793ac5d9358fe
FrancisDavison <francis.nj.davison@gmail.com>
2021-07-31 09:24:19 +0100

    Added basic functionality code

--- src/mian.java
+++ src/mian.java
@@ -2,6 +2,25 @@ public class mian
 {
 	public static void main(String args[])
 	{
-		
+		long input=7;
+		long temp=input;
+		System.out.println(temp);
+		while(temp!=1)
+		{
+			if(temp==1)
+			{
+				break;
+			}
+			if(temp%2!=0)
+			{
+				temp=(3*temp)+1;
+				System.out.println(temp);
+			}
+			if(temp%2==0)
+			{
+				temp=temp/2;
+				System.out.println(temp);
+			}
+		}
 	}
 }
